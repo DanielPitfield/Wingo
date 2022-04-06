8e417e8ee8830380eec08a9b54bc25a3ac0bbd6d
Benj <benjo>
2022-04-06 19:33:23 +0100

    Filter lists
    Co-authored-by: Daniel Pitfield <danielpitfield1@gmail.com>

--- WordArrays/Categories/jobs.ts
+++
@@ -1,3 +0,0 @@
-export {}
-
-// TODO: https://www.myworldofwork.co.uk/my-career-options/job-categories
--- WordArrays/Categories/pizza_toppings.ts
+++
@@ -1,5 +0,0 @@
-export {}
-
-// TODO: https://www.recipepizza.com/toppings/
-
-// TODO: https://www.ranker.com/list/the-tastiest-pizza-toppings/chef-jen
