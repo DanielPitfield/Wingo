d149f6aaab784e0c01b968ace689d5eccdaf14ff
Benj <benjo>
2022-04-18 14:42:32 +0100

    Background splash page

--- src/SplashScreen.tsx
+++ src/SplashScreen.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import {Logo} from "./Logo";
+import { Logo } from "./Logo";
+import BackgroundSrc from "./images/background.png";
 
 export const SplashScreen: React.FC<{ loadingState: "loading" | "loaded" }> = (props) => {
   return (
-    <div className="splash-screen">
+    <div className="splash-screen" style={{ backgroundImage: `url(${BackgroundSrc})` }}>
       <div className="main-menu-wrapper" data-loading-state={props.loadingState}>
         <div className="game-name">
           <div className="game-name-label">
