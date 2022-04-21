b12b35df7b8858f2ebe0e49813df9f4e23ef039a
Daniel Pitfield <danielpitfield1@gmail.com>
2022-04-21 02:51:46 +0100

    Keyboard - add space bar

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -21,8 +21,12 @@ export const alphabet_string = "abcdefghijklmnopqrstuvwxyz";
 export const Alphabet = alphabet_string.split("");
 
 export const Keyboard: React.FC<Props> = (props) => {
-  const modesWithDash = ["category", "letters_categories"];
-  const modesWithoutKeyboardStatuses = ["letters_categories", "countdown_letters_casual", "countdown_letters_realistic"];
+  const modesWithSpaces = ["category", "letters_categories"];
+  const modesWithoutKeyboardStatuses = [
+    "letters_categories",
+    "countdown_letters_casual",
+    "countdown_letters_realistic",
+  ];
 
   React.useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -35,9 +39,12 @@ export const Keyboard: React.FC<Props> = (props) => {
       } else if (Alphabet.includes(input_key)) {
         // Any letter on the keyboard
         props.onSubmitLetter(input_key);
-      } else if (modesWithDash.includes(props.mode)) {
-        // Allow dash and apostrophe for category gamemode
-        if (input_key === "-" || input_key === "'") {
+      } else if (modesWithSpaces.includes(props.mode)) {
+        // Space or dash pressed, submit dash
+        if (input_key === " " || input_key === "-") {
+          props.onSubmitLetter("-");
+        }
+        if (input_key === "'") {
           props.onSubmitLetter(input_key);
         }
       }
@@ -55,12 +62,14 @@ export const Keyboard: React.FC<Props> = (props) => {
       status: "not set",
     }));
 
-    if (modesWithDash.includes(props.mode)) {
-      keyboardStatuses.push({ letter: "-", status: "not set" });
+    if (modesWithSpaces.includes(props.mode)) {
+      // There may be multiples spaces in a word, best not to create a status (always be shown as "not set")
+
+      // Unlikely a word has multiple apostrophes, so showing a keyboard status is useful
       keyboardStatuses.push({ letter: "'", status: "not set" });
     }
 
-    // Don't need updated keyboard statuses if Countdown Letters
+    // Don't need updated keyboard statuses for some modes
     if (modesWithoutKeyboardStatuses.includes(props.mode)) {
       // Just use standard statuses (where all are "not set")
       return keyboardStatuses;
@@ -103,21 +112,32 @@ export const Keyboard: React.FC<Props> = (props) => {
     const keyboardStatuses = getKeyboardStatuses();
 
     for (let i = 0; i < RowLetters.length; i++) {
+      // Individual letter from RowString
+      const letter = RowLetters[i];
+      // If space, show the word space instead
+      const displayText = letter === " " ? "Space" : letter;
       // Find status using keyboardStatuses (correct, not in word, wrong position)
-      const letterStatus = keyboardStatuses.find((x) => x.letter.toUpperCase() === RowLetters[i].toUpperCase())?.status;
+      const letterStatus = keyboardStatuses.find((x) => x.letter.toUpperCase() === letter.toUpperCase())?.status;
 
       KeyboardButtons.push(
         <Button
           key={i}
+          className={`keyboard_${displayText}`}
           mode="default"
           // Data attribute used to colour button
-          status={letterStatus}
-          onClick={(e) =>
+          status={letterStatus ? letterStatus : "not set"}
+          onClick={(e) => {
+            // The display text of the clicked button
+            const buttonText = (e.target as HTMLButtonElement).innerText;
+            // All the ways the space bar may be shown on the keyboard
+            const Blankspace_keys = ["Space", "SPACE", " ", ""];
+            // If button was space, submit a dash
+            const letterToSubmit =  Blankspace_keys.includes(buttonText) ? "-" : buttonText;
             // Letter of button is used within a callback function
-            props.onSubmitLetter((e.target as HTMLButtonElement).innerText)
-          }
+            props.onSubmitLetter(letterToSubmit);
+          }}
         >
-          {RowLetters[i]}
+          {displayText}
         </Button>
       );
     }
@@ -128,10 +148,10 @@ export const Keyboard: React.FC<Props> = (props) => {
   return (
     <div className="keyboard_wrapper">
       <div className="keyboard_row_top">
-        <>{populateKeyboard(props.mode !== "category" ? "QWERTYUIOP" : "QWERTYUIOP-")}</>
+        <>{populateKeyboard("QWERTYUIOP")}</>
       </div>
       <div className="keyboard_row_middle">
-        <>{populateKeyboard(props.mode !== "category" ? "ASDFGHJKL" : "ASDFGHJKL'")}</>
+        <>{populateKeyboard(!modesWithSpaces.includes(props.mode) ? "ASDFGHJKL" : "ASDFGHJKL'")}</>
       </div>
       <div className="keyboard_row_bottom">
         <>{populateKeyboard("ZXCVBNM")}</>
@@ -139,6 +159,7 @@ export const Keyboard: React.FC<Props> = (props) => {
           &lt;
         </Button>
       </div>
+      <div className="keyboard_space">{modesWithSpaces.includes(props.mode) && (<>{populateKeyboard(" ")}</>)}</div>
       <div className="keyboard_enter">
         <Button mode="accept" onClick={props.onEnter}>
           Enter
