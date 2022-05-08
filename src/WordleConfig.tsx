696b016f4468bdfffea6d34b060be012fc81eb5a
Benj <benjo>
2022-05-08 10:09:56 +0100

    Use deterministic package

--- src/WordleConfig.tsx
+++ src/WordleConfig.tsx
@@ -27,6 +27,7 @@ import { words_animals } from "./WordArrays/Categories/animals";
 import { words_herbs_and_spices } from "./WordArrays/Categories/herbs_and_spices";
 import { words_meats_and_fish } from "./WordArrays/Categories/meats_and_fish";
 import { words_gemstones } from "./WordArrays/Categories/gemstones";
+import { Chance } from "chance";
 
 export interface WordleConfigProps {
   mode: "daily" | "repeat" | "category" | "increasing" | "limitless" | "puzzle" | "interlinked" | "crossword";
@@ -174,6 +175,64 @@ export function getLetterStatus(
   return status;
 }
 
+/**
+ * Gets a number guaranteed to be the same throughout today, and guaranteed to change every day.
+ * @returns Seed value.
+ */
+function todaySeed(): number {
+  return Number(
+    new Date().toLocaleDateString("en-GB", { year: "numeric", month: "2-digit", day: "2-digit" }).replace(/[^\d]/g, "")
+  );
+}
+
+/**
+ * Gets a number guaranteed to be the same for all days of this week, and guaranteed to change every Monday.
+ * @returns Seed value.
+ */
+function thisWeekSeed(): number {
+  const mondayThisWeek = new Date();
+
+  while (mondayThisWeek.getDay() !== 1) {
+    mondayThisWeek.setDate(mondayThisWeek.getDate() - 1);
+  }
+
+  return Number(
+    mondayThisWeek
+      .toLocaleDateString("en-GB", { year: "numeric", month: "2-digit", day: "2-digit" })
+      .replace(/[^\d]/g, "")
+  );
+}
+
+/**
+ * Gets a number of items from an array, guaranteed to be deterministic based on the seed.
+ * @param seed Seed of the returned items.
+ * @param numItems Number of items to return.
+ * @param array Array of possible items.
+ * @returns Deterministic items from array as per the seed.
+ */
+function getDeterministicArrayItems<T>(
+  seed: { seedType: "today" } | { seedType: "this-week" } | { seedType: "custom"; value: number },
+  numItems: number,
+  array: T[]
+): T[] {
+  const seedValue = (() => {
+    switch (seed.seedType) {
+      case "today":
+        return todaySeed();
+
+      case "this-week":
+        return thisWeekSeed();
+
+      case "custom":
+        return seed.value;
+    }
+  })();
+
+  const chance = new Chance(seedValue);
+
+  return chance.shuffle(array.slice()).slice(0, numItems);
+}
+
 const WordleConfig: React.FC<Props> = (props) => {
   const [guesses, setGuesses] = useState<string[]>(props.guesses ?? []);
   const [numGuesses, setNumGuesses] = useState(props.defaultnumGuesses);
@@ -244,14 +303,9 @@ const WordleConfig: React.FC<Props> = (props) => {
 
     switch (props.mode) {
       case "daily":
-        const timestamp = +new Date(); // Unix timestamp (in milliseconds)
-        const ms_per_day = 24 * 60 * 60 * 1000;
-        const days_since_epoch = Math.floor(timestamp / ms_per_day);
-        const daily_word_index = Math.round(days_since_epoch % targetWordArray.length); // Number in the range (0, wordArray.length)
-
-        newTarget = targetWordArray[daily_word_index];
+        newTarget = getDeterministicArrayItems({ seedType: "today" }, 1, targetWordArray)[0];
         console.log(
-          `%cMode:%c daily\n%cHint:%c ${newTarget.hint}\n%cWord:%c ${newTarget.word}`,
+          `%cMode:%c ${props.mode}\n%cHint:%c ${newTarget.hint}\n%cWord:%c ${newTarget.word}`,
           "font-weight: bold",
           "font-weight: normal",
           "font-weight: bold",
@@ -264,6 +318,7 @@ const WordleConfig: React.FC<Props> = (props) => {
 
         // Load previous attempts at daily (if applicable)
         const daily_word_storage = SaveData.getDailyWordGuesses();
+
         // The actual daily word and the daily word set in local storage are the same
         if (newTarget.word === daily_word_storage?.dailyWord) {
           // Display the sava data on the word grid
@@ -820,9 +875,9 @@ const WordleConfig: React.FC<Props> = (props) => {
   if (props.mode === "interlinked") {
     return (
       <WordleInterlinked
-      wordArrayConfig={{type: "length"}}
-      displayHints={false}
-      provideWords={false}
+        wordArrayConfig={{ type: "length" }}
+        displayHints={false}
+        provideWords={false}
         numWords={2}
         minWordLength={props.defaultWordLength ? props.defaultWordLength : 5}
         maxWordLength={props.defaultWordLength ? props.defaultWordLength : 5}
@@ -836,7 +891,7 @@ const WordleConfig: React.FC<Props> = (props) => {
   if (props.mode === "crossword") {
     return (
       <WordleInterlinked
-        wordArrayConfig={{type: "category"}}
+        wordArrayConfig={{ type: "category" }}
         displayHints={true}
         provideWords={false}
         numWords={6}
