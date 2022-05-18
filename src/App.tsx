141c5cfe393c788ab3c175550e7a6efedfa76d0b
Benj <benjo>
2022-05-18 18:56:48 +0100

    Add some more helpInfos

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,15 @@ export type Page =
   | "lingo/gameshow";
 
 // This is needed for runtime; make sure it matches the Page type
-export const pages: { page: Page; title: string; description?: string; shortTitle?: string; isPlayable: boolean }[] = [
+// // TODO: Every gamemode has this helpInfo component, use state with callback to determine whether it is currently displayed or not
+export const pages: {
+  page: Page;
+  title: string;
+  description?: string;
+  shortTitle?: string;
+  isPlayable: boolean;
+  helpInfo?: JSX.Element;
+}[] = [
   { page: "splash-screen", title: "Wingo", isPlayable: false },
   { page: "home", title: "", isPlayable: false },
   {
@@ -96,6 +104,13 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Guess today's word",
     shortTitle: "Daily",
     isPlayable: false,
+    helpInfo: (
+      <>
+        <p>Only one attempt (a set of up to 6 guesses) allowed</p>
+        <p>The target word changes every day (the countdown timer shows when a new word is available)</p>
+        <p>Your attempt will be saved and can be viewed at any time</p>
+      </>
+    ),
   },
   {
     page: "wingo/repeat",
@@ -103,6 +118,11 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Guess a word",
     shortTitle: "Standard",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>Press the 'Restart' button after an attempt for a new target word</p>
+      </>
+    ),
   },
   {
     page: "wingo/category",
@@ -110,6 +130,16 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Guess a word related to a category",
     shortTitle: "Categories",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>The target word is a word from the currently selected category</p>
+        <p>
+          The guesses you make must also be words from this category (but they do not have to be the length of the
+          target word)
+        </p>
+        <p>The category can be changed from the dropdown list (this will delete any guesses made)</p>
+      </>
+    ),
   },
   {
     page: "wingo/increasing",
@@ -117,6 +147,11 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Increase the word length to guess with every correct answer",
     shortTitle: "Increasing",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>The target word will increase in length (one letter longer) after each successful guess</p>
+      </>
+    ),
   },
   {
     page: "wingo/limitless",
@@ -124,6 +159,13 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Gain lives with correct, early answers; how long can you survive?",
     shortTitle: "Limitless",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>Gain extra guesses (up to a maximum of 5) by guessing a word with guesses to spare</p>
+        <p>A guess will be lost each time the target word is not guessed</p>
+        <p>The target word will increase in length (one letter longer) after each successful guess</p>
+      </>
+    ),
   },
   {
     page: "wingo/puzzle",
@@ -131,6 +173,13 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Use a cryptic clue to guess the word as fast as possible!",
     shortTitle: "Puzzle",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>Guess the target word from the hint provided</p>
+        <p>A random letter of the target word will become revealed every few seconds</p>
+        <p>Press 'Enter' once you know the answer and make your guess (this will stop the letters revealing!)</p>
+      </>
+    ),
   },
   {
     page: "wingo/interlinked",
@@ -138,6 +187,14 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Guess two words interlinked by a shared letter",
     shortTitle: "Interlinked",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>Guess the two target words, using the shared letter as a hint</p>
+        <p>Click on the word to highlight it, and make a guess</p>
+        <p>Click 'Check Crossword' once you have guessed both words to see if your guesses are correct</p>
+        <p>Do all this without running out of guesses!</p>
+      </>
+    ),
   },
   {
     page: "wingo/crossword",
@@ -145,6 +202,15 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Guess a crossword of words",
     shortTitle: "Crossword",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>Guess the many target words, using the shared letters as hints</p>
+        <p>Click on the word to highlight it, and make a guess</p>
+        <p>Click 'Check Current Word' once you have guessed one word to see which letters are correct</p>
+        <p>Click 'Check Crossword' once you have guessed all words to see if your guesses are correct</p>
+        <p>Do all this without running out of guesses!</p>
+      </>
+    ),
   },
   {
     page: "wingo/crossword/fit",
@@ -152,6 +218,14 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Fill the crossword with the provided words",
     shortTitle: "Crossword Fit",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>Fill each word with one of the provided words, using the revealed letters as hints</p>
+        <p>Click on the word to highlight it, and make a guess</p>
+        <p>Click 'Check Crossword' once you have guessed all words to see if your guesses are correct</p>
+        <p>Do all this without running out of guesses!</p>
+      </>
+    ),
   },
   {
     page: "wingo/crossword/weekly",
@@ -159,6 +233,16 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Guess a crossword for this week",
     shortTitle: "Weekly Crossword",
     isPlayable: false,
+    helpInfo: (
+      <>
+        <p>Complete a crossword specifically for this week</p>
+        <p>Guess the many target words, using the shared letters as hints</p>
+        <p>Click on the word to highlight it, and make a guess</p>
+        <p>Click 'Check Current Word' once you have guessed one word to see which letters are correct</p>
+        <p>Click 'Check Crossword' once you have guessed all words to see if your guesses are correct</p>
+        <p>Do all this without running out of guesses!</p>
+      </>
+    ),
   },
   {
     page: "wingo/crossword/daily",
@@ -166,6 +250,16 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Guess a crossword for today",
     shortTitle: "Daily Crossword",
     isPlayable: false,
+    helpInfo: (
+      <>
+        <p>Complete a crossword specifically for today only!</p>
+        <p>Guess the many target words, using the shared letters as hints</p>
+        <p>Click on the word to highlight it, and make a guess</p>
+        <p>Click 'Check Current Word' once you have guessed one word to see which letters are correct</p>
+        <p>Click 'Check Crossword' once you have guessed all words to see if your guesses are correct</p>
+        <p>Do all this without running out of guesses!</p>
+      </>
+    ),
   },
   {
     page: "letters_categories",
@@ -173,6 +267,13 @@ export const pages: { page: Page; title: string; description?: string; shortTitl
     description: "Guess the word for each category",
     shortTitle: "Categories (5)",
     isPlayable: true,
+    helpInfo: (
+      <>
+        <p>Select a category</p>
+        <p>Guess the word from the hint for the category within the number of guesses</p>
+        <p>Press the 'Restart' button after an attempt or change the Category for a new target word</p>
+      </>
+    ),
   },
   {
     page: "countdown/letters",
@@ -577,7 +678,7 @@ export const App: React.FC = () => {
              * @param value Checkbox checked (true) or not checked (false)
              * @param Page The page for the game type which options have just changed
              */
-             wordLengthValue={(value, page) =>
+            wordLengthValue={(value, page) =>
               setGameOptionToggles(
                 gameOptionToggles.map((gameOption) => ({
                   ...gameOption,
@@ -1090,10 +1191,10 @@ export const App: React.FC = () => {
           />
         );
 
-        case "verbal_reasoning/word_codes/match":
+      case "verbal_reasoning/word_codes/match":
         return (
           <WordCodes
-            modeConfig={{isMatch: true}}
+            modeConfig={{ isMatch: true }}
             numWords={4}
             wordLength={5}
             numAdditionalLetters={2}
@@ -1176,10 +1277,10 @@ export const App: React.FC = () => {
               </nav>
             )}
             <h1 className="title">{pages.find((x) => x.page === page)?.title}</h1>
-            {Boolean(page !== "home" && page !== "settings") && (
+            {Boolean(pages.find((x) => x.page === page)?.helpInfo) && (
               <Button
                 mode="default"
-                className="game_options"
+                className="help-info-button"
                 settings={settings}
                 onClick={() => setIsHelpInfoShown(true)}
               >
