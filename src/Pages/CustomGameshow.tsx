c5e28c31ba57308e40516e38853b4622e3f254c0
Daniel Pitfield <danielpitfield1@gmail.com>
2022-08-14 02:43:20 +0100

    CustomGameshow - draggable order

--- src/Pages/CustomGameshow.tsx
+++ src/Pages/CustomGameshow.tsx
@@ -3,7 +3,7 @@ import { SettingsData } from "../Data/SaveData";
 import { PageName } from "../PageNames";
 import { Theme } from "../Data/Themes";
 import { pageDescriptions } from "../PageDescriptions";
-import { OrderGroup } from "react-draggable-order";
+import { arrayMove, OrderGroup } from "react-draggable-order";
 import { DraggableItem } from "../Components/DraggableItem";
 
 export interface CustomGameshowProps {
@@ -31,24 +31,39 @@ export type gameshowType = typeof gameshowTypes[number];
 
 export const CustomGameshow: React.FC<Props> = (props) => {
   const [currentGameshowType, setCurrentGameshowType] = useState<gameshowType>("Custom");
-  const [queuedModes, setQueuedModes] = useState(null);
+  const [queuedModes, setQueuedModes] = useState<
+    | {
+        page: PageName;
+        title: string;
+        shortTitle?: string;
+        isPlayable: boolean;
+        gameshowType?: gameshowType;
+        description?: string;
+        helpInfo?: JSX.Element;
+      }[]
+    | null
+  >(null);
 
-  function displayGamemodes() {
-    const modesToDisplay =
+  React.useEffect(() => {
+    const newQueuedModes =
       currentGameshowType === "Custom"
         ? // Any playable mode
           pageDescriptions.filter((page) => page.isPlayable)
         : // Any mode tagged with the current gameshow type
           pageDescriptions.filter((page) => page.gameshowType === currentGameshowType && page.isPlayable);
+          
+    setQueuedModes(newQueuedModes);
+  }, [currentGameshowType]);
 
+  function displayGamemodes() {
     return (
       <div className="gameshow-mode-wrapper">
         <OrderGroup mode={"between"}>
-          {modesToDisplay.map((gameshowMode, index) => (
+          {queuedModes?.map((gameshowMode, index) => (
             <DraggableItem
               key={index}
               index={index}
-              onMove={(toIndex) => {}/*setResultTiles(arrayMove(resultTiles, index, toIndex)) */}
+              onMove={(toIndex) => setQueuedModes(arrayMove(queuedModes, index, toIndex))}
             >
               <div className="gameshow-gamemode">{gameshowMode.title}</div>
             </DraggableItem>
