74b56c33727c54941b3d4beed8593de19c872b88
Daniel Pitfield <danielpitfield1@gmail.com>
2022-09-20 11:25:57 +0100

    getNumbersGameScore refactor

--- src/Helper Functions/getNumbersGameScore.ts
+++ src/Helper Functions/getNumbersGameScore.ts
@@ -1,58 +1,63 @@
+const getScoreFromDifference = (difference: number, scoringMethod: "standard" | "pointLostPerDifference") => {
+  // Is the difference between 0 and 10?
+  const withinTen = difference >= 0 && difference <= 10;
+
+  // If not within ten, zero score
+  if (!withinTen) {
+    return 0;
+  }
+
+  /* 
+  10 points for reaching it exactly
+  7 points for being 1–5 away
+  5 points for being 6–10 away
+  */
+  if (scoringMethod === "standard") {
+    const exactAnswer = difference === 0;
+    const sevenPointRange = difference >= 1 && difference <= 5;
+
+    if (exactAnswer) {
+      return 10;
+    }
+
+    if (sevenPointRange) {
+      return 7;
+    }
+
+    return 5;
+  }
+
+  if (scoringMethod === "pointLostPerDifference") {
+    // Award one point for being 10 away
+    if (difference === 10) {
+      return 1;
+    }
+
+    // 10 points for exact answer (and one point is lost for each additional one difference after that)
+    return 10 - difference;
+  }
+};
+
 export function getNumbersGameScore(
   bestGuess: number | null,
   targetNumber: number | null,
   scoringMethod: "standard" | "pointLostPerDifference"
 ): { score: number | null; difference: number | null } {
-    //TODO: Refactor
+  const noScore = { score: null, difference: null };
+
   if (!targetNumber) {
-    return { score: null, difference: null };
+    return noScore;
   }
 
   if (bestGuess === null) {
-    return { score: null, difference: null };
+    return noScore;
   }
 
   const difference = Math.abs(bestGuess - targetNumber);
 
   if (difference === null) {
-    return { score: null, difference: null };
+    return noScore;
   }
 
-  let score = 0;
-
-  /* 
-  Standard:
-  10 points for reaching it exactly
-  7 points for being 1–5 away
-  5 points for being 6–10 away
-  */
-
-  if (scoringMethod === "standard") {
-    const exactAnswer = difference === 0;
-    const sevenPointRange = difference >= 1 && difference <= 5;
-    const fivePointRange = difference >= 6 && difference <= 10;
-
-    if (exactAnswer) {
-      score = 10;
-    } else if (sevenPointRange) {
-      score = 7;
-    } else if (fivePointRange) {
-      score = 5;
-    } else {
-      score = 0;
-    }
-  } else if (scoringMethod === "pointLostPerDifference" && difference >= 0 && difference <= 10) {
-    // Award one point for being 10 away
-    if (difference === 10) {
-      score = 1;
-    }
-    // 10 points for exact answer (and one point is lost for each additional one difference after that)
-    else {
-      score = 10 - difference;
-    }
-  } else {
-    throw new Error("Unexpected NumbersGame scoring method");
-  }
-
-  return { score: score, difference: difference };
+  return { score: getScoreFromDifference(difference, scoringMethod) ?? 0, difference: difference };
 }
