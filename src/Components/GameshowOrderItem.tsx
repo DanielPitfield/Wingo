c44880d8730107fe10a4face8c8ecd119cd24150
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-04 10:41:08 +0100

    Refactor GameshowOrderItem sub-component (for dnd-kit)

--- src/Components/GameshowOrderItem.tsx
+++ src/Components/GameshowOrderItem.tsx
@@ -1,4 +1,3 @@
-import { arrayMove } from "react-draggable-order";
 import { AiFillCloseSquare } from "react-icons/ai";
 import { pageDescription } from "../Data/PageDescriptions";
 import { DraggableItem } from "./DraggableItem";
@@ -6,18 +5,14 @@ import { DraggableItem } from "./DraggableItem";
 interface Props {
   gameshowMode: pageDescription;
   index: number;
-  queuedModes: pageDescription[];
-  setQueuedModes: (queuedModes: pageDescription[]) => void;
   onClick: (index: number) => void;
 }
 
 export const GameshowOrderItem = (props: Props) => {
-  // TODO: Elements that allow configuration of the properties of the gamemode (e.g wordLength)
+  // TODO: Add more sub-elements that allow configuration of the properties of the gamemode (e.g wordLength)
   return (
     <DraggableItem
-      key={props.index}
-      index={props.index}
-      onMove={(toIndex) => props.setQueuedModes(arrayMove(props.queuedModes, props.index, toIndex))}
+      id={props.index}
     >
       <div className="gameshow-queued-gamemode">{props.gameshowMode.title}</div>
       <AiFillCloseSquare onClick={() => props.onClick(props.index)} />
