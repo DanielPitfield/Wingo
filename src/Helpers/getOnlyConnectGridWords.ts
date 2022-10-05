c9d5912bcd0830dcc9135d332363c2f6b8ba928a
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-05 15:06:02 +0100

    remove unneeded variables (OnlyConnect helper function)

--- src/Helpers/getOnlyConnectGridWords.ts
+++ src/Helpers/getOnlyConnectGridWords.ts
@@ -2,50 +2,55 @@ import { categoryMappings } from "../Data/WordArrayMappings";
 import { GridWord } from "../Pages/OnlyConnect";
 import { shuffleArray } from "./shuffleArray";
 
+// Is every word on the first row from the same category?
 const isFirstRowSameCategory = (gridWords: GridWord[]) => {
   return gridWords.slice(0, 3).every((word) => word.categoryName === gridWords[0].categoryName);
 };
 
+/* 
+The word 'Duck' can be from both the 'Meats and Fish' and 'Animals' categories,
+Need to add leeway (extra amount of words from a category's array) to accomodate for these kinds of replacements
+*/
+const NUM_WORD_REPLACEMENTS = 2;
+
 // (gamemodeSettings.groupSize) words from (gamemodeSettings.numGroups) categories, shuffled
 export function getOnlyConnectGridWords(numGroups: number, groupSize: number): GridWord[] {
-  // Array to hold all the words for the grid (along with their categories)
-  let gridWords: GridWord[] = [];
-
   // Use the specified number of categories (but never exceed the number of category word lists)
   const numCategories = Math.min(numGroups, categoryMappings.length);
+
   // Filter the categories which have enough enough words in their word list
   const suitableCategories = categoryMappings.filter(
-    (category) => category.array.length >= groupSize /* TODO: Leeway for word replacements? */
+    (category) => category.array.length >= groupSize + NUM_WORD_REPLACEMENTS
   );
+
   // Randomly select the required number of categories
   const chosenCategories = shuffleArray(suitableCategories).slice(0, numCategories);
 
+  let gridWords: GridWord[] = [];
+
   for (const category of chosenCategories) {
-    /* 
-      Words from this category (which aren't already included in gridWords from other categories)
-      e.g The word 'Duck' can be from both the 'Meats and Fish' and 'Animals' categories
-      May need to add leeway to the condition of suitableCategories to accomodate for these kinds of replacements
-      */
-    const wordList: string[] = shuffleArray(category.array)
+    const categorySubset: GridWord[] = shuffleArray(category.array)
+      // The words from the category's array
       .map((x) => x.word)
-      .filter((word) => !gridWords.map((x) => x.word).includes(word));
-    // The subset of words chosen from this category
-    const wordSubset = wordList.slice(0, groupSize);
+      // Filter the words which aren't already included in gridWords (came from other categories)
+      .filter((word) => !gridWords.map((x) => x.word).includes(word))
+      // The subset of words chosen
+      .slice(0, groupSize)
+      // Attach category name to every word (in an object)
+      .map((word) => ({
+        word: word,
+        categoryName: category.name,
+        inCompleteGroup: false,
+        rowNumber: null,
+      }));
 
-    // Attach category name to every word (in an object)
-    const categorySubset = wordSubset.map((word) => ({
-      word: word,
-      categoryName: category.name,
-      inCompleteGroup: false,
-      rowNumber: null,
-    }));
-
-    // Add group of words
     gridWords = gridWords.concat(categorySubset);
   }
 
+  // Initial shuffle (because the gridWords are created in the correct order)
   let shuffledWords = shuffleArray(gridWords);
 
+  // Keep shuffling until every word on the first row is not from the same category
   while (isFirstRowSameCategory(shuffledWords)) {
     shuffledWords = shuffleArray(gridWords);
   }
