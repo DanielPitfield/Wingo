f9650b4ca7ad8a409bc6dac49111ce54a390077f
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-06 05:42:09 +0100

    area backtrack path (fallback value)

--- src/Helpers/getAreaBacktrackPath.ts
+++ src/Helpers/getAreaBacktrackPath.ts
@@ -1,10 +1,16 @@
 import { PagePath } from "../Data/PageNames";
 import { isCampaignAreaPath, isCampaignLevelPath } from "./CampaignPathChecks";
 
-export function getAreaBacktrackPath(path: PagePath) {
-  // To go back to area, it must be a campaign level path
+export function getAreaBacktrackPath(path: PagePath): PagePath {
+  // Already an area path
+  if (isCampaignAreaPath(path)) {
+    return path;
+  }
+
+  // To backtrack to area, it must be a campaign level path
   if (!isCampaignLevelPath(path)) {
-    return isCampaignAreaPath(path) ? path : ("/Campaign" as PagePath);
+    // Not a campaign level path, go back to campaign page
+    return "/Campaign" as PagePath;
   }
 
   const pathString = path.toString();
@@ -16,7 +22,8 @@ export function getAreaBacktrackPath(path: PagePath) {
   const numDirectoriesToTrim = 2;
   // Remove the last two sub-directories
   directories.splice(directories.length - numDirectoriesToTrim, numDirectoriesToTrim);
-
   // Reconstruct the trimmed path
-  return directories.join("/") as PagePath;
+  const areaBacktrackPath = directories.join("/");
+
+  return areaBacktrackPath as PagePath ?? "/Campaign" as PagePath;
 }
