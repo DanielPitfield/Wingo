14bec1f3f54d495b07c8f89a955e5f76271aabe9
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-10 12:38:55 +0100

    Fix Wingo puzzle display row status

--- src/Components/WordRow.tsx
+++ src/Components/WordRow.tsx
@@ -1,6 +1,6 @@
-import LetterTile from "./LetterTile";
+import LetterTile, { LetterStatus } from "./LetterTile";
 import { SettingsData } from "../Data/SaveData";
-import { getWordRowStatusSummary, WordRowStatusChecks } from "../Helpers/getWordRowStatusSummary";
+import { getWordRowStatusSummary, WordRowStatusChecks, WordRowStatusSummary } from "../Helpers/getWordRowStatusSummary";
 import { useLocation } from "react-router-dom";
 import { PagePath } from "../Data/PageNames";
 
@@ -23,19 +23,7 @@ interface Props {
 export const WordRow = (props: Props) => {
   const location = useLocation().pathname as PagePath;
 
-  const statusChecks: WordRowStatusChecks = {
-    isReadOnly: props.isReadOnly,
-    page: location,
-    word: props.word,
-    targetWord: props.targetWord,
-    inDictionary: props.inDictionary,
-    wordArray: props.targetArray ?? [],
-  };
-
-  // Array of (character, status) for every letter
-  const guessSummary = getWordRowStatusSummary(statusChecks);
-
-  function isAnimationEnabled(letterIndex: number): boolean {
+  const isAnimationEnabled = (letterIndex: number): boolean => {
     if (location === "/LettersGame") {
       return false;
     }
@@ -58,14 +46,51 @@ export const WordRow = (props: Props) => {
     }
 
     return true;
-  }
+  };
+
+  const getGuessSummary = (): WordRowStatusSummary => {
+    const statusChecks: WordRowStatusChecks = {
+      isReadOnly: props.isReadOnly,
+      page: location,
+      word: props.word,
+      targetWord: props.targetWord,
+      inDictionary: props.inDictionary,
+      wordArray: props.targetArray ?? [],
+    };
+
+    // Array of (character, status) for every letter
+    return getWordRowStatusSummary(statusChecks);
+  };
+
+  const areAllStatusesCorrect = (): boolean => {
+    return getGuessSummary().every((letter) => letter.status === "correct");
+  };
+
+  const getFinalTileStatus = (index: number): LetterStatus => {
+    const guessSummary = getGuessSummary();
+
+    if (!guessSummary) {
+      return "not set";
+    }
+
+    // No word has been submitted for this WordRow yet
+    if (!props.hasSubmit || !props.word) {
+      return "not set";
+    }
+
+    // Unrevealed letters of the display row in Puzzle mode
+    if (location === "/Wingo/Puzzle" && props.isReadOnly && guessSummary[index].character === " ") {
+      return "not set";
+    }
+
+    // Otherwise, return the status at that index in the guessSummary
+    return guessSummary[index]?.status ?? "not set";
+  };
 
   function CreateRow() {
     let tileArray = [];
 
     for (let i = 0; i < props.length; i++) {
-      const allCorrect = guessSummary.every((letter) => letter.status === "correct");
-
       tileArray.push(
         <LetterTile
           key={i}
@@ -79,9 +104,9 @@ export const WordRow = (props: Props) => {
 
           If the word is correct, show a faster animation
           */
-          animationDelayMultiplier={props.revealedLetterIndexes ? 0 : allCorrect ? 0.3 : undefined}
+          animationDelayMultiplier={props.revealedLetterIndexes ? 0 : areAllStatusesCorrect() ? 0.3 : undefined}
           settings={props.settings}
-          status={!props.hasSubmit || !props.word ? "not set" : guessSummary[i]?.status}
+          status={getFinalTileStatus(i)}
         ></LetterTile>
       );
     }
