a233268444ead4ab83958fb9e13696743e9aaae0
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-13 14:35:51 +0100

    Empty set .has() fix

--- src/Components/LevelNode.tsx
+++ src/Components/LevelNode.tsx
@@ -36,23 +36,22 @@ export const LevelNode = (props: LevelNodeProps) => {
 
   const areaInfo = campaignProgress.areas.find((x) => x.name === props.area.name);
 
-  console.log("Area Info: " + areaInfo);
-
   // Determine whether this is the first level
   const isFirstLevel = props.levelNumber === 1;
 
   // Find the previous level (unless this is the first level)
   const previousLevel = isFirstLevel ? undefined : props.area.levels[props.levelNumber - 1];
 
-  console.log("Area Info - Completed Levels: " + areaInfo?.completedLevelNumbers);
+  // The completed levels for the area (that this LevelNode belongs to)
+  const completedLevels: string[] = Array.from(areaInfo?.completedLevelNumbers ?? []);
 
   // Determine whether the level has already been completed
-  const isLevelCompleted = Array.from(areaInfo?.completedLevelNumbers || []).includes(props.levelNumber.toString());
+  const isLevelCompleted = completedLevels.includes(props.levelNumber.toString());
 
   // Determine whether the level has been unlocked (i.e. the previous level has been completed)
   const isLevelUnlocked = !previousLevel
     ? areaInfo?.status === "unlocked"
-    : areaInfo?.completedLevelNumbers.has((props.levelNumber - 1).toString());
+    : completedLevels.includes((props.levelNumber - 1).toString());
 
   // Get the level page info
   const levelInfo = pageDescriptions.find((x) => x.path === props.level.level.page);
