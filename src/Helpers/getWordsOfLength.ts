9835375bae2e436bb75d031b854d773d4e6f4724
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-14 13:26:58 +0100

    getAllWordsUpToLength()

--- src/Helpers/getWordsOfLength.ts
+++ src/Helpers/getWordsOfLength.ts
@@ -1,3 +1,4 @@
+import { MIN_TARGET_WORD_LENGTH } from "../Data/GamemodeSettingsInputLimits";
 import {
   guessableWordLengthMappings,
   puzzleWordLengthMappings,
@@ -14,6 +15,19 @@ export function getTargetWordsOfLength(wordLength: number): string[] {
 
 export function getAllWordsOfLength(wordLength: number): string[] {
   return getGuessableWordsOfLength(wordLength).concat(getTargetWordsOfLength(wordLength)) ?? [];
+}
+
+export function getAllWordsUpToLength(wordLength: number): string[] {
+  // TODO: Refactor
+  let newWordArray: string[] = [];
+
+  for (let i = MIN_TARGET_WORD_LENGTH; i <= wordLength; i++) {
+    // Find array containing words of i length
+    const currentLengthWordArray = getAllWordsOfLength(i);
+    newWordArray = newWordArray.concat(currentLengthWordArray);
+  }
+
+  return newWordArray;
 }
 
 export function getAllPuzzleWordsOfLength(wordLength: number): { word: string; hint: string }[] {
