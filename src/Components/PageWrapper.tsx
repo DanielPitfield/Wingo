480b046445c185c8e28b43215d7ca8c2c23fa4e5
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-20 05:38:18 +0100

    ErrorBoundary fix - must be the top-level component

--- src/Components/PageWrapper.tsx
+++ src/Components/PageWrapper.tsx
@@ -31,85 +31,85 @@ export const PageWrapper = (props: Props) => {
   const [isHelpInfoShown, setIsHelpInfoShown] = useState(false);
 
   return (
-    <div className="app" data-automation-id="app" data-automation-page-name={location}>
-      {location !== "/Splashscreen" && location !== "/TitlePage" && (
-        <>
-          <div className="toolbar">
-            {location !== "/Home" && (
-              <nav className="navigation">
+    <ErrorBoundary
+      fallbackRender={({ error, resetErrorBoundary }) => (
+        <ErrorFallback
+          error={error}
+          resetErrorBoundary={resetErrorBoundary}
+          settingsData={getSettings()}
+        ></ErrorFallback>
+      )}
+      onReset={() => window.location.reload()}
+    >
+      <div className="app" data-automation-id="app" data-automation-page-name={location}>
+        {location !== "/Splashscreen" && location !== "/TitlePage" && (
+          <>
+            <div className="toolbar">
+              {location !== "/Home" && (
+                <nav className="navigation">
+                  <Button
+                    mode="default"
+                    className="back-button"
+                    settings={props.settings}
+                    onClick={() => {
+                      setIsHelpInfoShown(false);
+
+                      if (isCampaignLevelPath(location)) {
+                        navigate(getAreaBacktrackPath(location));
+                      } else if (isCampaignAreaPath(location)) {
+                        navigate("/Campaign");
+                      } else {
+                        navigate("/Home");
+                      }
+                    }}
+                  >
+                    <FiArrowLeft /> Back
+                  </Button>
+                </nav>
+              )}
+
+              <h1 className="title">{wrappedPage?.title}</h1>
+
+              {wrappedPage?.helpInfo !== undefined && (
                 <Button
                   mode="default"
-                  className="back-button"
+                  className="help-info-button"
                   settings={props.settings}
-                  onClick={() => {
-                    setIsHelpInfoShown(false);
-
-                    if (isCampaignLevelPath(location)) {
-                      navigate(getAreaBacktrackPath(location));
-                    } else if (isCampaignAreaPath(location)) {
-                      navigate("/Campaign");
-                    } else {
-                      navigate("/Home");
-                    }
-                  }}
+                  onClick={() => setIsHelpInfoShown(true)}
+                  additionalProps={{ "aria-label": "help", title: "Get help with this game mode" }}
                 >
-                  <FiArrowLeft /> Back
+                  <FiHelpCircle /> Help
                 </Button>
-              </nav>
-            )}
+              )}
 
-            <h1 className="title">{wrappedPage?.title}</h1>
-
-            {wrappedPage?.helpInfo !== undefined && (
               <Button
                 mode="default"
-                className="help-info-button"
+                className="props.settings-button"
                 settings={props.settings}
-                onClick={() => setIsHelpInfoShown(true)}
-                additionalProps={{ "aria-label": "help", title: "Get help with this game mode" }}
+                onClick={() => {
+                  navigate("/Settings");
+                  setIsHelpInfoShown(false);
+                }}
               >
-                <FiHelpCircle /> Help
+                <FiSettings /> Settings
               </Button>
-            )}
 
-            <Button
-              mode="default"
-              className="props.settings-button"
-              settings={props.settings}
-              onClick={() => {
-                navigate("/Settings");
-                setIsHelpInfoShown(false);
-              }}
-            >
-              <FiSettings /> Settings
-            </Button>
-
-            <div className="gold_counter" onClick={() => navigate("/Challenges")}>
-              <img className="gold_coin_image" src={GoldCoin} alt="Gold" />
-              {props.gold.toLocaleString("en-GB")}
+              <div className="gold_counter" onClick={() => navigate("/Challenges")}>
+                <img className="gold_coin_image" src={GoldCoin} alt="Gold" />
+                {props.gold.toLocaleString("en-GB")}
+              </div>
             </div>
-          </div>
-        </>
-      )}
-
-      {props.children}
-
-      <ErrorBoundary
-        fallbackRender={({ error, resetErrorBoundary }) => (
-          <ErrorFallback
-            error={error}
-            resetErrorBoundary={resetErrorBoundary}
-            settingsData={getSettings()}
-          ></ErrorFallback>
+          </>
         )}
-        onReset={() => window.location.reload()}
-      ></ErrorBoundary>
 
-      {isHelpInfoShown && wrappedPage?.helpInfo !== undefined && (
-        <HelpInformation onClose={() => setIsHelpInfoShown(false)}></HelpInformation>
-      )}
+        {props.children}
 
-      <div className="version">{VERSION}</div>
-    </div>
+        {isHelpInfoShown && wrappedPage?.helpInfo !== undefined && (
+          <HelpInformation onClose={() => setIsHelpInfoShown(false)}></HelpInformation>
+        )}
+
+        <div className="version">{VERSION}</div>
+      </div>
+    </ErrorBoundary>
   );
 };
