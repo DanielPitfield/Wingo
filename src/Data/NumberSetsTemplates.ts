efb4db3f0627868d59acfd310b29e67fe33c126e
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-09 04:59:20 +0000

    few more number set templates

--- src/Data/NumberSetsTemplates.ts
+++ src/Data/NumberSetsTemplates.ts
@@ -83,7 +83,95 @@ export const NumberSetsTemplates: NumberSetTemplate[] = [
     question: {
       numbersLeft: [smallNumbers[4]],
       numbersRight: [smallNumbers[5]],
-      correctAnswer:  smallNumbers[4] - smallNumbers[5],
+      correctAnswer: smallNumbers[4] - smallNumbers[5],
+    },
+  },
+
+  {
+    difficulty: "medium",
+    correctAnswerDescription: "Double the first, add the second",
+    examples: [
+      {
+        numbersLeft: [mediumNumbers[0]],
+        numbersRight: [mediumNumbers[1]],
+        correctAnswer: mediumNumbers[0] * 2 + mediumNumbers[1],
+      },
+      {
+        numbersLeft: [mediumNumbers[2]],
+        numbersRight: [mediumNumbers[3]],
+        correctAnswer: mediumNumbers[2] * 2 + mediumNumbers[3],
+      },
+    ],
+    question: {
+      numbersLeft: [mediumNumbers[4]],
+      numbersRight: [mediumNumbers[5]],
+      correctAnswer: mediumNumbers[4] * 2 + mediumNumbers[5],
+    },
+  },
+
+  {
+    difficulty: "medium",
+    correctAnswerDescription: "Multiply first by double the second",
+    examples: [
+      {
+        numbersLeft: [mediumNumbers[7]],
+        numbersRight: [smallNumbers[3]],
+        correctAnswer: mediumNumbers[7] * (smallNumbers[3] * 2),
+      },
+      {
+        numbersLeft: [mediumNumbers[2]],
+        numbersRight: [smallNumbers[5]],
+        correctAnswer: mediumNumbers[2] * (smallNumbers[5] * 2),
+      },
+    ],
+    question: {
+      numbersLeft: [mediumNumbers[4]],
+      numbersRight: [smallNumbers[1]],
+      correctAnswer: mediumNumbers[4] * (smallNumbers[1] * 2),
+    },
+  },
+
+  {
+    difficulty: "hard",
+    correctAnswerDescription: "Product of left multiplied by by product of right",
+    examples: [
+      {
+        numbersLeft: [smallNumbers[2], smallNumbers[3]],
+        numbersRight: [smallNumbers[5], smallNumbers[6]],
+        correctAnswer: smallNumbers[2] * smallNumbers[3] * (smallNumbers[5] * smallNumbers[6]),
+      },
+      {
+        numbersLeft: [smallNumbers[6], smallNumbers[7]],
+        numbersRight: [smallNumbers[8], smallNumbers[9]],
+        correctAnswer: smallNumbers[6] * smallNumbers[7] * (smallNumbers[8] * smallNumbers[9]),
+      },
+    ],
+    question: {
+      numbersLeft: [smallNumbers[4], smallNumbers[5]],
+      numbersRight: [smallNumbers[1], smallNumbers[9]],
+      correctAnswer: smallNumbers[4] * smallNumbers[5] * (smallNumbers[1] * smallNumbers[9]),
+    },
+  },
+
+  {
+    difficulty: "hard",
+    correctAnswerDescription: "Inner of each side added together * outside of each side added together",
+    examples: [
+      {
+        numbersLeft: [smallNumbers[7], smallNumbers[2]],
+        numbersRight: [smallNumbers[5], smallNumbers[4]],
+        correctAnswer: (smallNumbers[2] + smallNumbers[5]) * (smallNumbers[7] + smallNumbers[4]),
+      },
+      {
+        numbersLeft: [smallNumbers[3], smallNumbers[2]],
+        numbersRight: [smallNumbers[3], smallNumbers[5]],
+        correctAnswer: smallNumbers[2] + smallNumbers[3] * (smallNumbers[3] + smallNumbers[5]),
+      },
+    ],
+    question: {
+      numbersLeft: [smallNumbers[4], smallNumbers[6]],
+      numbersRight: [smallNumbers[5], smallNumbers[9]],
+      correctAnswer: (smallNumbers[6] + smallNumbers[5]) + (smallNumbers[4] + smallNumbers[9]),
     },
   },
 ];
