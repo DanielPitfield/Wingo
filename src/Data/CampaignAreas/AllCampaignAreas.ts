84b77e491df910f3333ed62dfc9a8fc738d67ee5
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-10 01:48:11 +0000

    Remove Areas 5 and 6 (empty)

--- src/Data/CampaignAreas/AllCampaignAreas.ts
+++ src/Data/CampaignAreas/AllCampaignAreas.ts
@@ -3,8 +3,6 @@ import { area as Area1 } from "./1-Tutorial";
 import { area as Area2 } from "./2-Travel";
 import { area as Area3 } from "./3-Space";
 import { area as Area4 } from "./4-Cars";
-import { area as Area5 } from "./5-Fantasy";
-import { area as Area6 } from "./6-Nature";
 
 // Define the game areas
-export const AllCampaignAreas: AreaConfig[] = [Area1, Area2, Area3, Area4, Area5, Area6];
+export const AllCampaignAreas: AreaConfig[] = [Area1, Area2, Area3, Area4];
