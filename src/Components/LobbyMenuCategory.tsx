ed8eb01e95663122c2e95721ff2762181fef01b2
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-13 22:56:01 +0000

    invert no category gamemodes check

--- src/Components/LobbyMenuCategory.tsx
+++ src/Components/LobbyMenuCategory.tsx
@@ -14,33 +14,32 @@ interface LobbyMenuCategoryProps {
 const LobbyMenuCategory = (props: LobbyMenuCategoryProps) => {
   const navigate = useNavigate();
 
-  // If there are gamemodes for this category, render a sidebar with the category name
-  if (props.categoryGamemodesPages.length > 0) {
-    return (
-      <div className="sidebar" key={props.category}>
-        <div className="sidebar-title">{props.category}</div>
-        <ul className="widgets">
-          <Carousel
-            onClickItem={(index: number, _) => navigate(props.categoryGamemodesPages[index].path)}
-            showIndicators={false}
-            showThumbs={false}
-            showStatus={false}
-            centerMode
-            centerSlidePercentage={33}
-            useKeyboardArrows
-            infiniteLoop
-          >
-            {props.categoryGamemodesPages.map((page) => {
-              return <LobbyMenuTile key={page.title} page={page} settings={props.settings} />;
-            })}
-          </Carousel>
-        </ul>
-      </div>
-    );
+  // No gamemodes for this category, don't render a carousel
+  if (!props.categoryGamemodesPages) {
+    return null;
   }
 
-  // No gamemodes for this category, don't even render a sidebar for the category
-  return null;
+  return (
+    <div className="sidebar" key={props.category}>
+      <div className="sidebar-title">{props.category}</div>
+      <ul className="widgets">
+        <Carousel
+          onClickItem={(index: number, _) => navigate(props.categoryGamemodesPages[index].path)}
+          showIndicators={false}
+          showThumbs={false}
+          showStatus={false}
+          centerMode
+          centerSlidePercentage={33}
+          useKeyboardArrows
+          infiniteLoop
+        >
+          {props.categoryGamemodesPages.map((page) => {
+            return <LobbyMenuTile key={page.title} page={page} settings={props.settings} />;
+          })}
+        </Carousel>
+      </ul>
+    </div>
+  );
 };
 
 export default LobbyMenuCategory;
